c4b92bf08f3e96acdaa15393922a4daff0474ab5
lyf118811 <lyf118811@gmail.com>
2018-05-23 23:11:46 -0400

    add mongodb

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 var authRoutes = require('./routes/index');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/personal-website');
+mongoose.connect('mongodb://yifanli:199322@ds133920.mlab.com:33920/personal-introduce-website');
 
-
+// mongodb://<yifanli>:<lyf199322>@ds133920.mlab.com:33920/personal-introduce-website
 
 
 // open view file and reduce ejs
@@ -24,10 +24,12 @@ var projectSchema = new mongoose.Schema({
 
 var projects = mongoose.model('Project',projectSchema);
 
-var campground
+// var campground;
+
 // add css and javascript path
 app.use(express.static(__dirname + '/public'));
 // add listener
-app.listen(4000,function(){
-    console.log('personal-page has started!');
+
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
